96df1aee851050887ea299b3ef7ac50ab08284c3
Umar Arif <umararif66@gmail.com>
2021-09-05 12:32:11 +0100

    Completed code for lab 2 with comments

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,5 +1,5 @@
 // CO2402: Advanced C++
-// Week 3: Introduction to classes, the counter class hello
+// Week 3: Introduction to classes, the counter class
 
 #include <iostream>
 
@@ -7,9 +7,11 @@
 class CCounter
 {
 private:
-	int mAmount; // Stores the current count value
-	int mTop = 0;
+	int mAmount = 0;       // Stores the current count value
+	int mTop = 0;          // Keeps a record of the number of operations
 public:
+	CCounter();            // Primary constructor
+	CCounter(int number);  // Constructor that sets mAmount to number
 	void Set(int number);  // Set count to the value of number
 	int  Get(); 		   // Get the current value of amount
 	void Increment();  	   // Increment count by 1 (trivial)
@@ -18,6 +20,17 @@ public:
 	void DisplayValues();  // Displays both mAmount and mTop
 	void TheRainInSpain(); // Will count the number of times this is outputted to the console
 };
+
+// Exercise 5: Constructors
+CCounter::CCounter()
+{
+	mAmount = 0;
+}
+
+CCounter::CCounter(int number)
+{
+	mAmount = number;
+}
 
 // Set counter to the value of number
 void CCounter::Set(int number)
@@ -59,7 +72,7 @@ void CCounter::DisplayValues()
 //  Exercise 3: using the Counter class - Counting the number of operations
 void CCounter::TheRainInSpain()
 {
-	std::cout << "The rain in spain" << std::endl;
+	std::cout << "The rain in Spain" << std::endl;
 	Increment();
 }
 
@@ -90,7 +103,7 @@ void CStudent::Get()
 int main()
 {
 	// Exercise 1: starting with the Counter class
-	CCounter* myCount = new CCounter; // Declare object of type CCounter
+	CCounter* myCount = new CCounter(); // Declare object of type CCounter
 
 	myCount->Set(6); // Set the value to 6
 
@@ -115,18 +128,21 @@ int main()
 	std::cout << tmp << std::endl << std::endl;
 
 	// Exercise 3: using the Counter class 
-	CCounter* newCounter = new CCounter;
-	newCounter->Set(0);
-	for (int i = 0; i < 100; i++)
+	CCounter* newCounter = new CCounter(0);
+	for (int i = 0; i < 10; i++)
 	{
 		newCounter->TheRainInSpain();
 	}
 	newCounter->AssignTop();
 	newCounter->DisplayValues();
+	std::cout << std::endl << std::endl;
 
 	// Exercise 4: a Student class
+	CStudent* student = new CStudent("Markus", 87.98f);
+	student->Set("Mark", 87.98f);
+	student->Get();
+	std::cout << std::endl << std::endl;
 
-	delete (myCount, newCounter);
+	delete (myCount, newCounter, student);
 	system("pause");
 }
-
